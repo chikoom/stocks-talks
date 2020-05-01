7956abc6d3e76a7ac21a46797804d411349ed495
Chikoom <callforcopy@gmail.com>
2020-05-01 21:23:46 +0300

    added routes for views: / and stock_details

--- stockstalks/urls.py
+++ stockstalks/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from stocksbasic import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    path(r'^$', views.home, name='home'),
+    path(r'^stocks/(\d+)/', views.stock_details, name='stock_details'),
 ]
